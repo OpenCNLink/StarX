99a3ffb93564eae5d1397329fbba9ce84cb0f728
Jankie <qwq@awaish.link>
2024-01-14 01:14:43 +0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 import cryptology.discretization
 
-dr = cryptology.discretization.DiscretizationRandom(1, 4) 
+dr = cryptology.discretization.DiscretizationRandom(65, 122) 
 result = dr.get_discretized_random() 
 print(result)
