cd47e4aa5ed319db17f9c4a077ca5157b620a4b3
Jankie <qwq@awaish.link>
2024-01-14 01:16:53 +0800

    Add password maker test

---
+++ src/init.py
@@ -0,0 +1,9 @@
+import cryptology.discretization
+
+dr = cryptology.discretization.DiscretizationRandom(65, 122) 
+rawResult = dr.get_discretized_random() 
+password = ''
+for text in rawResult:
+    password += chr(text)
+
+print(password)
