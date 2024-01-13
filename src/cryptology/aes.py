838f1ca3425c89cb07b8dae9e0d13c12327f37c7
Jankie <qwq@awaish.link>
2024-01-14 01:32:51 +0800

    Add hpassword

--- src/cryptology/aes.py
+++ src/cryptology/aes.py
@@ -9,19 +9,19 @@ def add_to_16(text):
     text = text + ('\0' * add)
     return text.encode('utf-8')
 
-def encrypt(text):
-    key = '9999999999999999'.encode('utf-8')
+def encrypt(text,key):
+    keyx = key.encode('utf-8')
     mode = AES.MODE_CBC
     iv = b'qqqqqqqqqqqqqqqq'
     text = add_to_16(text)
-    cryptos = AES.new(key, mode, iv)
+    cryptos = AES.new(keyx, mode, iv)
     cipher_text = cryptos.encrypt(text)
     return b2a_hex(cipher_text)
 
-def decrypt(text):
-    key = '9999999999999999'.encode('utf-8')
+def decrypt(text,key):
+    keyx = key.encode('utf-8')
     iv = b'qqqqqqqqqqqqqqqq'
     mode = AES.MODE_CBC
-    cryptos = AES.new(key, mode, iv)
+    cryptos = AES.new(keyx, mode, iv)
     plain_text = cryptos.decrypt(a2b_hex(text))
     return bytes.decode(plain_text).rstrip('\0')
--- src/init.py
+++ src/init.py
@@ -1,5 +1,6 @@
 import system.info
 import cryptology.aes
+import cryptology.md5
 import cryptology.discretization
 
 dr = cryptology.discretization.DiscretizationRandom(65, 122) 
@@ -7,6 +8,6 @@ rawResult = dr.get_discretized_random()
 password = ''
 for text in rawResult:
     password += chr(text)
-hpassword = cryptology.aes.encrypt(password)
+hpassword = cryptology.aes.encrypt(password,cryptology.md5.get_md5(str(system.info.sysInfo)))
+hpassword = hpassword.decode('utf-8','ignore')
 del password
-
