4e48b8ecb6ea896094fcd85e67463899c500c36d
Jankie <qwq@awaish.link>
2024-01-14 12:19:38 +0800

    Add function for printMessage

--- src/logger/output.py
+++ src/logger/output.py
@@ -3,3 +3,7 @@ import time
 def formatMessage(text) -> str:
     template = '[INFO] [{}] {}'
     return template.format(str(int(time.time())),text)
+
+def printMessage(text) -> None:
+    print(formatMessage(text))
+    return
