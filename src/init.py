cfa50de069c064374c286c273b69abc9eeb9e973
Jankie <qwq@awaish.link>
2024-01-26 18:56:57 +0800

    Add no-server(offline) mode

--- src/init.py
+++ src/init.py
@@ -43,10 +43,13 @@ while True:
         handshake = json.dumps(handshake)
         handshake = handshake.encode('utf-8')  # 将字符串转换为字节流
         headers = {'Accept-Charset': 'utf-8', 'Content-Type': 'application/json'}
-        for i in range(int(str(int(time.time()))[0])):
-            req = urllib.request.Request(url=server+'/api/v2/handshake', data=handshake, headers=headers, method='POST')
-            response = urllib.request.urlopen(req).read()
-        if not bool(response):raise RuntimeError('Cannot client to StarX server')
+        try:
+            for i in range(int(str(int(time.time()))[0])):
+                req = urllib.request.Request(url=server+'/api/v2/handshake', data=handshake, headers=headers, method='POST')
+                response = urllib.request.urlopen(req).read()
+        except:
+            pass
+        if not bool(response):showLog.print('与服务器握手失败，现在以离线模式启动 StarX.')
         from daemon.daemon import start as daemon
         daemon()
         showLog.print('守护进程已创建，现在请不要关闭 StarX 否则这会导致您的电脑崩溃。')
